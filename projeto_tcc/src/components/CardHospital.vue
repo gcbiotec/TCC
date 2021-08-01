1383f559bd8d948bda7cdb68550406ea9ae4d648
gabriel <gcbiotec@gmail.com>
2021-08-01 01:18:27 -0300

    Adicionando card hospital e link Tela de Cadastro

---
+++ projeto_tcc/src/components/CardHospital.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="image">
+    <img alt="imagem do hospital" src="../assets/hospital.png" />
+    <h2>{{ titulo }}</h2>
+    <h4>{{ subtitulo }}</h4>
+  </div>  
+</template>
+
+<script>
+export default {
+    name: "CardHospital",
+    props:{
+        titulo: String,
+        subtitulo: String,
+    },
+};
+</script>
+
+<style>
+
+</style>
--- projeto_tcc/src/views/Home.vue
+++ projeto_tcc/src/views/Home.vue
@@ -1,14 +1,34 @@
 <template>
-  
+  <div>
     <div class="card">
       <div class="card-body">
         <card-boas-vindas
           titulo="Bem vindo ao Data Clinic"
           subtitulo="A maneira mais fácil de entrar no mundo dos dados!"
         />
+        <card-hospital
+          titulo="Hospital de Pediatria"
+          subtitulo="Hospital do Carinho!"
+        />
       </div>
     </div>
-  
+
+    <div class="row">
+      <div class="col-6" id="Cadastro">
+        <button
+          @click="IrParaCadastro"
+          type="button"
+          class="btn btn-outline-success"
+        >
+          <card-cadastro
+            titulo="Cadastro de Cachorros"
+            subtitulo="Clique aqui para cadastrar um cachorro!"
+          />
+        </button>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -16,11 +36,18 @@
 //import HelloWorld from '@/components/HelloWorld.vue'
 
 import CardBoasVindas from "@/components/CardBoasVindas.vue";
+import CardHospital from '../components/CardHospital.vue';
 
 export default {
   name: 'Home',
   components: {
-   CardBoasVindas
+    CardBoasVindas,
+    CardHospital,
+  },
+  methods:{
+    IrParaCadastro(){
+      this.$router.push({path:"/TelaCadastro"});
+    }
   }
-}
+};
 </script>
