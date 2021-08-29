6a63dde242a162fcce7f6985f39404bbf20279b6
gabriel <gcbiotec@gmail.com>
2021-08-29 19:43:03 -0300

    Arrumando tela de Cadastro

--- projeto_tcc/src/views/TelaCadastro.vue
+++ projeto_tcc/src/views/TelaCadastro.vue
@@ -1,21 +1,28 @@
 <template>
   <div>
-    <img src="../assets/icon-patient.png" />
-
-    <h2>Cadastro do Paciente</h2>
-    <h4>Cadastre os dados do seu paciente!</h4>
-
-    <form class="needs-validation" novalidate>
-      <div id="campoNome" class="container">
-        <input
-          id="validationDefault01"
-          v-model="paciente.nome"
-          type="text"
-          class="form-control"
-          placeholder="Qual é o nome do paciente?"
-        />
+    <div class="row">
+      <div class="col-3"></div>
+      <div class="col-6">
+        <img id="imagem" src="../assets/icon-patient.png" />
+        <h2>Cadastro do Paciente</h2>
+        <h4>Cadastre os dados do seu paciente!</h4>
       </div>
-    </form>
+      <div class="col-3"></div>
+    </div>
+
+    <div class="row">
+      <form class="needs-validation" novalidate>
+        <div id="campoDeNome" class="container">
+          <input
+            id="validationDefault01"
+            v-model="paciente.nome"
+            type="text"
+            class="form-control"
+            placeholder="Qual é o nome do paciente?"
+          />
+        </div>
+      </form>
+    </div>
 
     <div class="container">
       <div class="row">
@@ -31,54 +38,51 @@
             <option value="Insuf. Mitral">Insuf. Mitral</option>
           </select>
         </div>
-
-        <div class="col-3">
-          <form>
-            <label id="escolhaPeso" for="formControlPeso"
-              >Escolha o peso:</label
-            >
-            <input
-              id="validationDefault02"
-              v-model="paciente.peso"
-              type="number"
-              value="1"
-              min="1"
-              max="100"
-              step="1"
-            />
-          </form>
-        </div>
-
-        <div class="col-3">
-          <form>
-            <label id="escolhaIdade" for="formControlIdade"
-              >Escolha a idade:</label
-            >
-            <input
-              id="validationDefault03"
-              v-model="paciente.idade"
-              type="number"
-              value="1"
-              min="1"
-              max="110"
-              step="1"
-            />
-          </form>
-        </div>
       </div>
-    </div>
-
-    <div id="botao" class="container">
       <div class="row">
-        <div class="col-4"></div>
-        <div class="col-4">
-          <button type="button" class="btn btn-primary" @click="salvar()">
-            Salvar
-          </button>
-          <div>
-            <span v-if="mensagemErro != ' '">{{ mensagemErro }}</span>
-          </div>
+        <form>
+          <label id="escolhaPeso" for="formControlPeso">Escolha o peso:</label>
+          <input
+            id="validationDefault02"
+            v-model="paciente.peso"
+            type="number"
+            value="1"
+            min="1"
+            max="100"
+            step="1"
+          />
+        </form>
+      </div>
+
+      <div class="row">
+        <form>
+          <label id="escolhaIdade" for="formControlIdade"
+            >Escolha a idade:</label
+          >
+          <input
+            id="validationDefault03"
+            v-model="paciente.idade"
+            type="number"
+            value="1"
+            min="1"
+            max="110"
+            step="1"
+          />
+        </form>
+      </div>
+
+      <div id="botao" class="container">
+        <div class="row">
           <div class="col-4"></div>
+          <div class="col-4">
+            <button type="button" class="btn btn-primary" @click="salvar()">
+              Salvar
+            </button>
+            <div>
+              <span v-if="mensagemErro != ' '">{{ mensagemErro }}</span>
+            </div>
+            <div class="col-4"></div>
+          </div>
         </div>
       </div>
     </div>
@@ -93,7 +97,7 @@ export default {
       paciente: {},
 
       mensagemErro: "",
-      listaVeterinarios: [],
+      
     };
   },
   // mounted() {
@@ -142,28 +146,51 @@ export default {
 </script>
 
 <style scoped>
-#campoNome {
-  width: 80%;
-  padding: 20pt;
+#imagem {
+  height: 200px;
+  width: 60%;
+  align-items: center;
+  background-color: powderblue;
+  padding-top: 40pt;
 }
-#campoVet {
+
+#campoDeNome {
   display: flex;
+  width: 70%;
   flex-direction: column;
+  padding-top: 40pt;
+  text-align: left;
+  align-items: left;
+
 }
+
 #botao {
   align-content: bottom;
   padding-top: 40pt;
 }
-#escolhaRaca {
+/* #escolhaRaca {
   display: flex;
   flex-direction: column;
 }
 #escolhaPeso {
   display: flex;
-  flex-direction: column;
-}
+  flex-direction: column; */
+
 #escolhaIdade {
   display: flex;
   flex-direction: column;
+  padding-top: 40pt;
+}
+
+#escolhaPeso{
+  display: flex;
+  flex-direction: column;
+  padding-top: 40pt;
+}
+
+#escolhaDiagnostico {
+  display: flex;
+  flex-direction: column;
+  padding-top: 40pt;
 }
 </style>
