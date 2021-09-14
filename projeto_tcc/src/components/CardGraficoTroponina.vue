e3858410857842453d30d87bae1bd378b472d790
gabriel <gcbiotec@gmail.com>
2021-09-14 01:00:11 -0300

    Cards de gráficos separados

---
+++ projeto_tcc/src/components/CardGraficoTroponina.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="Grafico">
+    <div id="titulos">
+    <h3>{{titulo}}</h3>
+    <h5>{{subtitulo}}</h5>
+    </div>
+
+     <visualisation-troponina></visualisation-troponina>
+
+  </div>  
+</template>
+
+<script>
+import VisualisationTroponina from "@/components/VisualisationTroponina.vue";
+
+export default {
+    name: 'CardGrafico',
+    props: {
+    titulo: String,
+    subtitulo: String,
+  },
+
+  components: {
+      VisualisationTroponina
+      },
+    
+}
+</script>
+
+<style>
+
+</style>
